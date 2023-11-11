b50301505c2405b3c895130a3e1dcbc58039e19f
dalchek <dalchek999@gmail.com>
2023-11-11 15:41:18 +0100

    Fix price formatting and radio selectors

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,8 @@
-import {addToCart, cart} from '../data/cart.js'
-import {products} from '../data/products.js'
+import {addToCart, cart} from '../data/cart.js';
+import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
-let productsHTML = ''
+let productsHTML = '';
 
 products.forEach((product) => {
     productsHTML += `
@@ -29,8 +30,8 @@ products.forEach((product) => {
                 }</div>
             </div>
 
-            <div class="product-price">$${(product.priceCents / 100).toFixed(
-                2
+            <div class="product-price">$${formatCurrency(
+                product.priceCents
             )}</div>
 
             <div class="product-quantity-container">
@@ -61,25 +62,25 @@ products.forEach((product) => {
                 Add to Cart
             </button>
         </div>
-    `
-})
+    `;
+});
 
-document.querySelector('.js-products-grid').innerHTML = productsHTML
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 function updateCartQuantity() {
-    let cartQuantity = 0
+    let cartQuantity = 0;
 
     cart.forEach((cartItem) => {
-        cartQuantity += cartItem.quantity
-    })
+        cartQuantity += cartItem.quantity;
+    });
 
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 }
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => {
-        const productId = button.dataset.productId
-        addToCart(productId)
-        updateCartQuantity()
-    })
-})
+        const productId = button.dataset.productId;
+        addToCart(productId);
+        updateCartQuantity();
+    });
+});
